c7ff6fff561de9dc1e94d994969e19b8ada10cd5
sharifhabiman <hdidiersharif@gmail.com>
2022-03-08 11:12:42 +0200

    simple change

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,30 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DATETIME
 
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    """Attributes:
+        id (sqlalchemy String): The BaseModel id.
+        created_at (sqlalchemy DateTime): The datetime at creation.
+        updated_at (sqlalchemy DateTime): The datetime of last update.
+    """
+    id = Column(String(60),
+                nullable=False,
+                primary_key=True,
+                unique=True)
+    created_at = Column(DATETIME,
+                        nullable=False,
+                        default=datetime.utcnow())
+    updated_at = Column(DATETIME,
+                        nullable=False,
+                        default=datetime.utcnow())
+    
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
